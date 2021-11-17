5a88e083beec6a0e56a2503f78a097a9c988288d
mickwa <mickwadev@gmail.com>
2021-11-17 21:57:11 +0100

    looks like 2.3.0 ;)

--- Main.py
+++ Main.py
@@ -3,3 +3,4 @@ print('hello')
 # not sure what TensorFlow is installed.
 # Probably 2.7.0, but Python Interpreter shows 2.3.0 with option to upgrade to 2.6.0
 tensor1 = tf.ones([1,2,3])
+print(tf.__version__) # shows 2.3.0
