3868ead3e437889aad855885d1d7b011f0b7644e
mickwa <mickwadev@gmail.com>
2021-11-17 21:56:09 +0100

    Not sure what tf version is installed

--- Main.py
+++ Main.py
@@ -1,3 +1,5 @@
 import tensorflow as tf
 print('hello')
+# not sure what TensorFlow is installed.
+# Probably 2.7.0, but Python Interpreter shows 2.3.0 with option to upgrade to 2.6.0
 tensor1 = tf.ones([1,2,3])
